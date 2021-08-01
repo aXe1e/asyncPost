1661dd9aaf7a3fab7de7d880640f7fef50382695
aXe1e <axele@list.ru>
2021-08-01 16:56:43 +0500

    Start DZ

--- asyncPost/Program.cs
+++ asyncPost/Program.cs
@@ -9,37 +9,11 @@ namespace asyncPost
 {
     class Program
     {
-        static readonly HttpClient client = new();
-        private static readonly string file = "Result.txt";
-        private static readonly int StartPost = 4;
-        private static readonly int FinishPost = 13;
-
-
-        static async Task Main()
+       
+        static void Main()
         {
-            File.Delete(file);
-            for (int i = StartPost; i <= FinishPost; i++)
-            {
-                var resultPost = await GetPostAsync(i);
-                await File.AppendAllTextAsync(file, resultPost.ToString() + Environment.NewLine);
-            }
+            
         }
 
-        private static async Task<Post> GetPostAsync(int postId)
-        {
-            // Call asynchronous network methods in a try/catch block to handle exceptions.
-            try
-            {
-                var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{postId}");
-                using var responseBody = await response.Content.ReadAsStreamAsync();
-                return JsonSerializer.DeserializeAsync<Post>(responseBody).Result;
-            }
-            catch (HttpRequestException e)
-            {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
-                throw;
-            }
-        }
     }
 }
