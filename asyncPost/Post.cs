83f807f280e7a23e8191d9a3080c1d4a8fa408a9
aXe1e <axele@list.ru>
2021-08-01 16:49:56 +0500

    Complete DZ

---
+++ asyncPost/Post.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asyncPost
+{
+    public class Post
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        public override string ToString()
+        {
+            return $"{userId}\n{id}\n{title}\n{body}\n";
+        }
+    }
+}
--- asyncPost/Program.cs
+++ asyncPost/Program.cs
@@ -1,12 +1,44 @@
 ﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
 
 namespace asyncPost
 {
     class Program
     {
-        static void Main(string[] args)
+        static readonly HttpClient client = new();
+        private static readonly string file = "Result.txt";
+        private static readonly int StartPost = 4;
+        private static readonly int FinishPost = 13;
+
+        static async Task Main()
         {
-            
+            File.Delete(file);
+            for (int i = StartPost; i <= FinishPost; i++)
+            {
+                var resultPost = await GetPostAsync(i);
+                await File.AppendAllTextAsync(file, resultPost.ToString() + Environment.NewLine);
+            }
+        }
+
+        private static async Task<Post> GetPostAsync(int postId)
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{postId}");
+                using var responseBody = await response.Content.ReadAsStreamAsync();
+                return JsonSerializer.DeserializeAsync<Post>(responseBody).Result;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+                throw;
+            }
         }
     }
 }
